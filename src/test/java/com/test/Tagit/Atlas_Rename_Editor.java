7c9be5817a2f77fd07733f2d154f222ca0db7c20
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-02-18 15:27:43 +0530

    Atlas_Rename_Editor.java

--- src/test/java/com/test/Tagit/Atlas_Rename_Editor.java
+++ src/test/java/com/test/Tagit/Atlas_Rename_Editor.java
@@ -36,10 +36,10 @@ public void Login() throws InterruptedException {
 
     WebDriverWait wait = new WebDriverWait(driver, 50);
     try {
-        WebElement viewerSectionLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@class, 'ng-tns-c93-3')]")));
-        viewerSectionLink.click();
-        System.out.println("--------------------------*****************-----------------------");
-        System.out.println("The Viewer Icon is clicked");
+        // WebElement viewerSectionLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@class, 'ng-tns-c93-3')]")));
+        // viewerSectionLink.click();
+        // System.out.println("--------------------------*****************-----------------------");
+        // System.out.println("The Viewer Icon is clicked");
 
         String parentWindow = driver.getWindowHandle();
         WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()=' Log In ']")));
